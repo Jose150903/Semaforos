34747e2f262d24524fa95befcaa386fe590b5e3a
Rog G75V <Rog G75V@JOSE-MONROY>
2024-09-30 19:06:47 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Simulacion_Semaforo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ptbVerde = new System.Windows.Forms.PictureBox();
+            this.ptbAmarillo = new System.Windows.Forms.PictureBox();
+            this.ptbRojo = new System.Windows.Forms.PictureBox();
+            this.ptbRojo1 = new System.Windows.Forms.PictureBox();
+            this.ptbAmarillo1 = new System.Windows.Forms.PictureBox();
+            this.ptbVerde1 = new System.Windows.Forms.PictureBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.txtVerde = new System.Windows.Forms.TextBox();
+            this.txtAmarillo = new System.Windows.Forms.TextBox();
+            this.txtRojo = new System.Windows.Forms.TextBox();
+            this.lblVerde = new System.Windows.Forms.Label();
+            this.lblAmarillo = new System.Windows.Forms.Label();
+            this.lblRojo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbVerde)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbAmarillo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbRojo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbRojo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbAmarillo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbVerde1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptbVerde
+            // 
+            this.ptbVerde.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ptbVerde.Location = new System.Drawing.Point(400, 338);
+            this.ptbVerde.Name = "ptbVerde";
+            this.ptbVerde.Size = new System.Drawing.Size(33, 33);
+            this.ptbVerde.TabIndex = 0;
+            this.ptbVerde.TabStop = false;
+            // 
+            // ptbAmarillo
+            // 
+            this.ptbAmarillo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ptbAmarillo.Location = new System.Drawing.Point(400, 301);
+            this.ptbAmarillo.Name = "ptbAmarillo";
+            this.ptbAmarillo.Size = new System.Drawing.Size(33, 33);
+            this.ptbAmarillo.TabIndex = 1;
+            this.ptbAmarillo.TabStop = false;
+            // 
+            // ptbRojo
+            // 
+            this.ptbRojo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ptbRojo.Location = new System.Drawing.Point(400, 264);
+            this.ptbRojo.Name = "ptbRojo";
+            this.ptbRojo.Size = new System.Drawing.Size(33, 33);
+            this.ptbRojo.TabIndex = 2;
+            this.ptbRojo.TabStop = false;
+            // 
+            // ptbRojo1
+            // 
+            this.ptbRojo1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ptbRojo1.Location = new System.Drawing.Point(243, 336);
+            this.ptbRojo1.Name = "ptbRojo1";
+            this.ptbRojo1.Size = new System.Drawing.Size(33, 33);
+            this.ptbRojo1.TabIndex = 3;
+            this.ptbRojo1.TabStop = false;
+            this.ptbRojo1.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // ptbAmarillo1
+            // 
+            this.ptbAmarillo1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ptbAmarillo1.Location = new System.Drawing.Point(205, 336);
+            this.ptbAmarillo1.Name = "ptbAmarillo1";
+            this.ptbAmarillo1.Size = new System.Drawing.Size(33, 33);
+            this.ptbAmarillo1.TabIndex = 4;
+            this.ptbAmarillo1.TabStop = false;
+            // 
+            // ptbVerde1
+            // 
+            this.ptbVerde1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ptbVerde1.Location = new System.Drawing.Point(167, 336);
+            this.ptbVerde1.Name = "ptbVerde1";
+            this.ptbVerde1.Size = new System.Drawing.Size(33, 33);
+            this.ptbVerde1.TabIndex = 5;
+            this.ptbVerde1.TabStop = false;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.Location = new System.Drawing.Point(101, 459);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 34);
+            this.btnIniciar.TabIndex = 6;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.BtnIniciar_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDetener.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDetener.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetener.Location = new System.Drawing.Point(258, 459);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(75, 34);
+            this.btnDetener.TabIndex = 7;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = false;
+            this.btnDetener.Click += new System.EventHandler(this.BtnDetener_Click);
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnApagar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnApagar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApagar.Location = new System.Drawing.Point(447, 459);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(75, 34);
+            this.btnApagar.TabIndex = 8;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = false;
+            this.btnApagar.Click += new System.EventHandler(this.BtnApagar_Click);
+            // 
+            // txtVerde
+            // 
+            this.txtVerde.Location = new System.Drawing.Point(108, 424);
+            this.txtVerde.Name = "txtVerde";
+            this.txtVerde.Size = new System.Drawing.Size(63, 20);
+            this.txtVerde.TabIndex = 9;
+            this.txtVerde.Text = "4";
+            // 
+            // txtAmarillo
+            // 
+            this.txtAmarillo.Location = new System.Drawing.Point(264, 424);
+            this.txtAmarillo.Name = "txtAmarillo";
+            this.txtAmarillo.Size = new System.Drawing.Size(63, 20);
+            this.txtAmarillo.TabIndex = 10;
+            this.txtAmarillo.Text = "4";
+            // 
+            // txtRojo
+            // 
+            this.txtRojo.Location = new System.Drawing.Point(456, 424);
+            this.txtRojo.Name = "txtRojo";
+            this.txtRojo.Size = new System.Drawing.Size(63, 20);
+            this.txtRojo.TabIndex = 11;
+            this.txtRojo.Text = "4";
+            // 
+            // lblVerde
+            // 
+            this.lblVerde.AutoSize = true;
+            this.lblVerde.BackColor = System.Drawing.Color.Transparent;
+            this.lblVerde.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVerde.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblVerde.Location = new System.Drawing.Point(100, 399);
+            this.lblVerde.Name = "lblVerde";
+            this.lblVerde.Size = new System.Drawing.Size(85, 13);
+            this.lblVerde.TabIndex = 12;
+            this.lblVerde.Text = "Tiempo Verde";
+            // 
+            // lblAmarillo
+            // 
+            this.lblAmarillo.AutoSize = true;
+            this.lblAmarillo.BackColor = System.Drawing.Color.Transparent;
+            this.lblAmarillo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmarillo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblAmarillo.Location = new System.Drawing.Point(249, 399);
+            this.lblAmarillo.Name = "lblAmarillo";
+            this.lblAmarillo.Size = new System.Drawing.Size(96, 13);
+            this.lblAmarillo.TabIndex = 13;
+            this.lblAmarillo.Text = "Tiempo Amarillo";
+            // 
+            // lblRojo
+            // 
+            this.lblRojo.AutoSize = true;
+            this.lblRojo.BackColor = System.Drawing.Color.Transparent;
+            this.lblRojo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRojo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblRojo.Location = new System.Drawing.Point(448, 399);
+            this.lblRojo.Name = "lblRojo";
+            this.lblRojo.Size = new System.Drawing.Size(78, 13);
+            this.lblRojo.TabIndex = 14;
+            this.lblRojo.Text = "Tiempo Rojo";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(397, 261);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 113);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(163, 334);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(116, 37);
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Simulacion_Semaforo.Properties.Resources.cruce_vial2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(626, 544);
+            this.Controls.Add(this.ptbVerde1);
+            this.Controls.Add(this.ptbAmarillo1);
+            this.Controls.Add(this.ptbRojo1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.ptbRojo);
+            this.Controls.Add(this.ptbAmarillo);
+            this.Controls.Add(this.ptbVerde);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblRojo);
+            this.Controls.Add(this.lblAmarillo);
+            this.Controls.Add(this.lblVerde);
+            this.Controls.Add(this.txtRojo);
+            this.Controls.Add(this.txtAmarillo);
+            this.Controls.Add(this.txtVerde);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnIniciar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbVerde)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbAmarillo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbRojo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbRojo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbAmarillo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbVerde1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptbVerde;
+        private System.Windows.Forms.PictureBox ptbAmarillo;
+        private System.Windows.Forms.PictureBox ptbRojo;
+        private System.Windows.Forms.PictureBox ptbRojo1;
+        private System.Windows.Forms.PictureBox ptbAmarillo1;
+        private System.Windows.Forms.PictureBox ptbVerde1;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.TextBox txtVerde;
+        private System.Windows.Forms.TextBox txtAmarillo;
+        private System.Windows.Forms.TextBox txtRojo;
+        private System.Windows.Forms.Label lblVerde;
+        private System.Windows.Forms.Label lblAmarillo;
+        private System.Windows.Forms.Label lblRojo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Simulacion_Semaforo
+{
+    public partial class Form1 : Form
+    {
+         Timer timer1 = new Timer();
+
+        // Tiempos configurables para los semáforos
+         int tiempoRojo, tiempoAmarillo, tiempoVerde;
+         int tiempoRojo1;
+         int tiempoAmarillo1; 
+         int tiempoVerde1;
+
+         int estadoSemaforo1 = 0; // 0: Rojo, 1: Amarillo, 2: Verde
+         int estadoSemaforo2 = 2; // Inicializado en 2 para que empiece en verde (opuesto al semáforo 1)
+
+        //Reproductor de sonido de cambio
+        SoundPlayer Sonido_Semaforo = new SoundPlayer("C:\\Users\\Rog G75V\\Desktop\\Sistemas Computacionales\\Simulacion_Semaforo\\Resources\\Sonido_Semaforo2.wav");
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+
+            timer1.Interval = 250; // 1 segundo
+            timer1.Tick += Timer1_Tick;
+
+            //Para hacer la forma de las luces redondas
+            luzCircular(ptbRojo);
+            luzCircular(ptbAmarillo);
+            luzCircular(ptbVerde);
+            luzCircular(ptbRojo1);
+            luzCircular(ptbAmarillo1);
+            luzCircular(ptbVerde1);
+            
+        }
+        //Método para hacer los picturebox circulares(luces)
+        private void luzCircular(PictureBox pictureBox)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(0,0, pictureBox.Width,pictureBox.Height);
+            pictureBox.Region = new Region(path);
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            try {
+                
+            // Lógica para cambiar el color del semáforo 1
+            switch (estadoSemaforo1)
+            {
+                case 0: // Rojo
+                    ptbRojo.BackColor = Color.Red;
+                    ptbAmarillo.BackColor = Color.Gray;
+                    ptbVerde.BackColor = Color.Gray;
+
+                    // Semáforo 2 en verde (opuesto)
+                    ptbRojo1.BackColor = Color.Gray;
+                    ptbAmarillo1.BackColor = Color.Gray;
+                    ptbVerde1.BackColor = Color.Green;
+
+                    if (tiempoRojo1 == 0)
+                    {
+
+                        estadoSemaforo1 = 1; // Cambia al estado amarillo
+                        estadoSemaforo2 = 1; // Cambia al estado amarillo
+                        tiempoRojo1 = (Convert.ToInt32(txtRojo.Text)*4); // Restablece tiempo de rojo
+                        //Sonido para el cambio de luces
+                        Sonido_Semaforo.Play();
+                    }
+                    tiempoRojo1--;
+                    break;
+
+                case 1: // Amarillo
+                    ptbRojo.BackColor = Color.Gray;
+                    ptbAmarillo.BackColor = Color.Yellow;
+                    ptbVerde.BackColor = Color.Gray;
+
+                    // Semáforo 2 en amarillo (sincronizado)
+                    ptbRojo1.BackColor = Color.Gray;
+                    ptbAmarillo1.BackColor = Color.Yellow;
+                    ptbVerde1.BackColor = Color.Gray;
+
+                    if (tiempoAmarillo1 == 0)
+                    {
+                        estadoSemaforo1 = 2; // Cambia al estado verde
+                        estadoSemaforo2 = 0; // Cambia al estado rojo (opuesto)
+                        tiempoAmarillo1 = (Convert.ToInt32(txtAmarillo.Text)*4); // Restablece tiempo de amarillo
+                        //Sonido para el cambio de luces
+                        Sonido_Semaforo.Play();
+                    }
+                    tiempoAmarillo1--;
+                    break;
+
+                case 2: // Verde
+                    ptbRojo.BackColor = Color.Gray;
+                    ptbAmarillo.BackColor = Color.Gray;
+                    ptbVerde.BackColor = Color.Green;
+
+                    // Semáforo 2 en rojo (opuesto)
+                    ptbRojo1.BackColor = Color.Red;
+                    ptbAmarillo1.BackColor = Color.Gray;
+                    ptbVerde1.BackColor = Color.Gray;
+
+                    if (tiempoVerde1 == 0)
+                    {
+                        estadoSemaforo1 = 0; // Cambia al estado rojo
+                        estadoSemaforo2 = 2; // Cambia al estado verde (opuesto)
+                        tiempoVerde1 = (Convert.ToInt32(txtVerde.Text) * 4); // Restablece tiempo de verde
+                        //Sonido para el cambio de luces
+                        Sonido_Semaforo.Play();
+                    }
+                    tiempoVerde1--;
+                    break;
+                }
+            }
+            catch
+            {
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Método para iniciar los semáforos
+        
+        private void BtnIniciar_Click(object sender, EventArgs e)
+        {
+            tiempoRojo1 = Convert.ToInt32(txtRojo.Text); // Restablece tiempo de rojo
+            tiempoAmarillo1 = Convert.ToInt32(txtAmarillo.Text); // Restablece tiempo de amarillo
+            tiempoVerde1 = Convert.ToInt32(txtVerde.Text); // Restablece tiempo de verde
+            timer1.Start();
+
+        }
+
+        // Método para detener los semáforos
+        private void BtnDetener_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        // Método para apagar los semáforos (pone todos los colores en gris)
+        private void BtnApagar_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            ptbRojo.BackColor = Color.Gray;
+            ptbAmarillo.BackColor = Color.Gray;
+            ptbVerde.BackColor = Color.Gray;
+
+            ptbRojo1.BackColor = Color.Gray;
+            ptbAmarillo1.BackColor = Color.Gray;
+            ptbVerde1.BackColor = Color.Gray;
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
